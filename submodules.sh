104a2b7b37f1e52556f2b3d723cada5f63974ff7
Austin Songer,MIS,CEH,ESCA,Project+ (Navy Veteran) <a.songer@protonmail.com>
2024-10-03 15:22:40 -0500

    Create submodules.sh

---
+++ submodules.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Add submodule for sql (case-insensitive)
+git submodule add https://github.com/austinsonger/SQL-scripts sql
+
+# Add submodule for vagrant-configs
+git submodule add https://github.com/austinsonger/Vagrant-templates vagrant-configs
+
+# Add submodule for packer
+git submodule add https://github.com/austinsonger/Packer packer
+
+# Add submodule for kubernetes-configs
+git submodule add https://github.com/austinsonger/Kubernetes-configs kubernetes-configs
+
+# Add submodule for templates
+git submodule add https://github.com/austinsonger/base-templates templates
+
+# Initialize and update the submodules
+git submodule init
+git submodule update
+
+# Stage the .gitmodules file and submodule directories
+git add .gitmodules SQL sql vagrant-configs packer kubernetes-configs templates
+
+# Commit the changes
+git commit -m "Add submodules for SQL, Vagrant, Packer, Kubernetes, and templates"
+
+# Push the changes to the repository
+git push
